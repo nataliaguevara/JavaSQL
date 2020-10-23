42292abd545e89b2222c9099a02fe94123e1ae5c
sara <sara.guevara@implementos.com>
2020-10-23 15:00:01 -0500

    se nmodifico 4

--- src/SQL/Conexion.java
+++ src/SQL/Conexion.java
@@ -28,7 +28,7 @@ public class Conexion {
                     pass = "";//epysa2014 cuando me conecto a mi local no le pongo pass okaz////////
                     
                     
-                    /*guardando**-sssssssssssssssssssss***/
+                    /*se guardo los datos*/
                     
                     
         try {
